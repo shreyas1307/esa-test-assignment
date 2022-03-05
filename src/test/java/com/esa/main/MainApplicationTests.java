f3b019774381f0494a9dd51c25b687077b1c804b
Shreyas Balachandran <shreyas.bba@gmail.com>
2022-03-06 02:02:31 +0530

    Added Main Test Classe

---
+++ src/test/java/com/esa/main/MainApplicationTests.java
@@ -0,0 +1,13 @@
+package com.esa.main;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class MainApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
